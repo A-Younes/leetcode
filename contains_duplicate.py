65e585cb41f99f46c81145952764251e88c3ea19
A-Younes <84933866+A-Younes@users.noreply.github.com>
2023-07-17 21:05:39 +0300

    Add files via upload

---
+++ contains_duplicate.py
@@ -0,0 +1,14 @@
+def contains_dup(num_list):
+    # create and empty hashset to add to when iterating over the list
+    hashset = set()
+
+    for i in num_list:
+        if i in hashset:
+            print("True")
+            return True
+        hashset.add(i)
+    print("False")
+    return False
+
+nums = [1, 2, 3, 4, 5, 6, 7, 1]
+contains_dup(nums)
